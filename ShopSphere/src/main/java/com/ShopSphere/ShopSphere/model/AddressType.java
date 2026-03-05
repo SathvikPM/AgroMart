74de8f5ce5b8abe63d4eb2a7a829433768d14d90
Sathvik PM <SATHVIKPAAROL2002@GMAIL.COM>
2026-03-05 21:49:03 +0530

    Project changed from Shopshere to AgriMart

--- ShopSphere/src/main/java/com/ShopSphere/ShopSphere/model/AddressType.java
+++
@@ -1,6 +0,0 @@
-package com.ShopSphere.ShopSphere.model;
-
-public enum AddressType {
-    HOME,
-    WORK
-}
--- ShopSphere/src/main/java/com/ShopSphere/ShopSphere/model/ProductStatus.java
+++
@@ -1,7 +0,0 @@
-package com.ShopSphere.ShopSphere.model;
-
-public enum ProductStatus {
-    ACTIVE,
-    INACTIVE,
-    OUT_OF_STOCK
-}
