ffac7f31dbc36d6e4b734a930eaf514f319ff8e2
Sathvik PM <SATHVIKPAAROL2002@GMAIL.COM>
2026-04-24 08:48:27 +0530

    Project changed from Shopshere to AgriMart

--- AgroMart/src/main/java/com/AgroMart/Backend/AgroMartApplication.java
+++ AgroMart/src/main/java/com/AgroMart/Backend/AgroMartApplication.java
@@ -2,13 +2,20 @@ package com.AgroMart.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class AgroMartApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(AgroMartApplication.class, args);
+
+		ApplicationContext context=SpringApplication.run(AgroMartApplication.class, args);
+//		String[] beans=context.getBeanDefinitionNames();
+//		for(String bean:beans){
+//			System.out.println(bean);
+//		}
 	}
 
 }
